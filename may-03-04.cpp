f9eb2b3f519a916a21ebda797ea5173cd32e9206
Reazul Islam <reazulislam137@gmail.com>
2018-05-03 19:27:40 +0600

    first commit

---
+++ may-03-04.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+#include<string>
+using namespace std;
+int main()
+{
+    double num1=10, num2=2;
+
+    string myString = "Test String";
+
+    cout << pow (num1, num2) << endl;
+
+    cout << sqrt(num1) << endl;
+
+    cout << myString.length()<< endl;
+
+    return 0;
+}
+
